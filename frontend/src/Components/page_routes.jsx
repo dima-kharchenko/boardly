71890d9219c495e20d08d8f56ffadd71bc89cecd
dima-kharchenko <dmitriy020606@gmail.com>
2026-02-03 16:30:37 +0100

    Add signup page

--- frontend/src/Components/page_routes.jsx
+++ frontend/src/Components/page_routes.jsx
@@ -1,11 +1,13 @@
 import Home from "../Pages/Home"
 import NotFound from "../Pages/NotFound"
 import Login from "../Pages/Login"
+import Signup from "../Pages/Signup"
 
 
 const page_routes = [
     { path: '/', page: Home, protected: true },
     { path: '/login', page: Login, protected: false },
+    { path: '/signup', page: Signup, protected: false },
     { path: '*', page: NotFound },  
 ];
 
